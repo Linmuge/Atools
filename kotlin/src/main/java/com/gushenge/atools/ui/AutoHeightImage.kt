a44b6c36762e26622777d7fdaf511ee05545f95f
Gushenge <cikexiaosi@gmail.com>
2019-09-10 15:43:07 +0800

    add license and javadoc

--- kotlin/src/main/java/com/gushenge/atools/ui/AutoHeightImage.kt
+++ kotlin/src/main/java/com/gushenge/atools/ui/AutoHeightImage.kt
@@ -3,6 +3,7 @@ package com.gushenge.atools.ui
 import android.content.Context
 import android.util.AttributeSet
 import androidx.appcompat.widget.AppCompatImageView
+import kotlin.math.ceil
 
 class AutoHeightImage : AppCompatImageView {
     constructor(context: Context) : super(context)
@@ -11,17 +12,14 @@ class AutoHeightImage : AppCompatImageView {
 
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
         val d = drawable
-
-        if (d != null) {
+        d?.let {
             // ceil not round - avoid thin vertical gaps along the left/right edges
             val width = MeasureSpec.getSize(widthMeasureSpec)
             //高度根据使得图片的宽度充满屏幕计算而得
-            val height =
-                Math.ceil((width.toFloat() * d.intrinsicHeight.toFloat() / d.intrinsicWidth.toFloat()).toDouble())
-                    .toInt()
+            val height = ceil((width.toFloat() * it.intrinsicHeight.toFloat() / it.intrinsicWidth.toFloat()).toDouble()).toInt()
             setMeasuredDimension(width, height)
-        } else {
-            super.onMeasure(widthMeasureSpec, heightMeasureSpec)
         }
+        d?:super.onMeasure(widthMeasureSpec, heightMeasureSpec)
+
     }
 }
