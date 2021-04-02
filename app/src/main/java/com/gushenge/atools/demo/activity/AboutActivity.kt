78c8397796d0f51271387b94f194da121b5668dd
Gushenge <cikexiaosi@gmail.com>
2021-04-02 17:27:16 +0800

    update kotlin and remove anko

--- app/src/main/java/com/gushenge/atools/demo/activity/AboutActivity.kt
+++ app/src/main/java/com/gushenge/atools/demo/activity/AboutActivity.kt
@@ -4,7 +4,6 @@ import android.graphics.Color
 import android.os.Bundle
 import android.view.View
 import com.gushenge.atools.demo.ui.titlebar
-import com.gushenge.atools.ui.arcButton
 import com.gushenge.atools.util.ARandom
 import com.gushenge.atools.util.AView
 import org.jetbrains.anko.*
@@ -14,17 +13,17 @@ class AboutActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        setContentView()
         verticalLayout {
-            titlebar("关于", View.VISIBLE).init(viewManager = this,activity = this@AboutActivity)
+            titlebar("关于", View.VISIBLE).init(viewManager = this, activity = this@AboutActivity)
             arcButton {
                 val color = ARandom.color()
-                textColor = if(AView.isLightColor(color))Color.BLACK else Color.WHITE
+                textColor = if (AView.isLightColor(color)) Color.BLACK else Color.WHITE
                 backgroundColor = color
                 text = "Github"
                 allCaps = false
                 onClick { browse("https://github.com/Gushenge") }
-            }.lparams{
+            }.lparams {
                 width = matchParent
                 height = dip(45)
                 margin = dip(5)
---
+++ app/src/main/java/com/gushenge/atools/demo/ui/Utils.kt
@@ -0,0 +1,19 @@
+import android.content.ActivityNotFoundException
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
+
+fun Context.browse(url: String, newTask: Boolean = false): Boolean {
+    try {
+        val intent = Intent(Intent.ACTION_VIEW)
+        intent.data = Uri.parse(url)
+        if (newTask) {
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+        }
+        startActivity(intent)
+        return true
+    } catch (e: ActivityNotFoundException) {
+        e.printStackTrace()
+        return false
+    }
+}
